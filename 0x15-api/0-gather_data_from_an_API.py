3da62db7177bd2f32e2efa55675e519de8046efa
John Mwega <93873207+CodeDroid999@users.noreply.github.com>
2023-05-22 18:10:23 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
-"""
-Returns TODO list progress for a given employee ID.
-"""
-
-import sys
+"""gets api"""
 import requests
+from sys import argv
+
+
+def todo(userid):
+    """doc stringed"""
+    name = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(
+            userid)).json().get('name')
+    tasks = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+            userid)).json()
+    tasksDone = ['\t {}\n'.format(dic.get('title')) for dic in tasks
+                 if dic.get('completed')]
+    if name and tasks:
+        print("Employee {} is done with tasks({}/{}):".format
+              (name, len(tasksDone), len(tasks)))
+        print(''.join(tasksDone), end='')
 
 
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-
-    try:
-        user_response = requests.get(user_url)
-        todos_response = requests.get(todos_url)
-
-        user_data = user_response.json()
-        todos_data = todos_response.json()
-
-        employee_name = user_data.get("name")
-        total_tasks = len(todos_data)
-        completed_tasks = [task.get("title") for task in todos_data if task.get("completed")]
-
-        print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
-        for task_title in completed_tasks:
-            print(f"\t {task_title}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+    if len(argv) == 2:
+        todo(int(argv[1]))
